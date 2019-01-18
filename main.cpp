588f464362e1e2e7cf7d6566a8fe0b0e8360c116
Kevin Veld <kevin.veld@student.hu.nl>
2019-01-18 23:17:59 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <sstream>
+
+std::string CaesarCipher(std::string string, int shift, bool cipher);
+
+int main(){
+  std::string string = "";
+  std::cout << "Type :q to exit" << std::endl;
+  while(1){
+    std::cout << " ? : ";
+    std::getline(std::cin, string);
+    if(string == ":q") break;
+
+    std::string encipherdStr = CaesarCipher(string, 5, true);
+    std::string decipheredStr = CaesarCipher(encipherdStr, 5, false);
+    std::cout << "Enciphered: " << encipherdStr << "\nDeciphered: " << decipheredStr << std::endl;
+  }
+  std::cout << "Exiting";
+  return 0;
+}
+
+std::string CaesarCipher(std::string string, int shift, bool cipher){
+  const int nAlphabet = 26;
+  std::string result;
+
+  if(!cipher) shift = nAlphabet - shift;
+  for(int i = 0; i < string.size(); ++i){
+    if(isspace(string[i])) result += ' ';
+    else if(islower(string[i])) result += char(((int)string[i] - (int)'a' + shift) % nAlphabet + (int)'a');
+    else if(isupper(string[i])) result += char(((int)string[i] - (int)'A' + shift) % nAlphabet + (int)'A');
+  }
+  return result;
+}
